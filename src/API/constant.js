1e451fefc5bc7349f253e33232168ea3ab537958
waleed naseem <waleed.naseem1@gmail.com>
2024-01-14 23:22:56 +0500

    api changed

--- src/API/constant.js
+++ src/API/constant.js
@@ -1,4 +1,5 @@
 // var apiUrl = 'https://www.privateadminapi.smartxblockchain.com';
-var apiUrl = 'http://192.168.1.104:8080';
+// var apiUrl = 'http://192.168.1.104:8080';
+var apiUrl = 'https://api.smartxblockchain.com';
 
 export default apiUrl;
